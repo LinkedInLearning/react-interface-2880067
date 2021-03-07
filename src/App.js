25861e72834eaa5392bd59399710b3f7560aa309
planetoftheweb <ray@planetoftheweb.com>
2021-03-07 14:49:41 -0500

    installed Tailwind

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { BiArchive } from "react-icons/bi"
 
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive />Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
     </div>
   );
 }
