8377d1337464f03e949ffde26f36f8e1ae30745f
planetoftheweb <ray@planetoftheweb.com>
2021-03-08 13:54:38 -0500

    Searching with Filtered Arrays

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import AppointmentInfo from "./components/AppointmentInfo"
 function App() {
 
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  )
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -25,10 +36,11 @@ function App() {
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
       <AddAppointment />
-      <Search />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)} />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList
+        {filteredAppointments
           .map(appointment => (
             <AppointmentInfo key={appointment.id}
               appointment={appointment}
--- src/components/Search.js
+++ src/components/Search.js
@@ -29,7 +29,7 @@ const DropDown = ({ toggle }) => {
   )
 }
 
-const Search = () => {
+const Search = ({ query, onQueryChange }) => {
   let [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
@@ -38,7 +38,8 @@ const Search = () => {
           <BiSearch />
           <label htmlFor="query" className="sr-only" />
         </div>
-        <input type="text" name="query" id="query" value=""
+        <input type="text" name="query" id="query" value={query}
+          onChange={(event) => { onQueryChange(event.target.value) }}
           className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
         <div className="absolute inset-y-0 right-0 flex items-center">
           <div>
