968b58716eff1ed8d295b7c8b10b2cad6eafbc47
planetoftheweb <ray@planetoftheweb.com>
2021-03-08 02:54:15 -0500

    passing state and hiding content

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,10 @@
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi"
+import { useState } from 'react';
 
-const DropDown = () => {
+const DropDown = ({ toggle }) => {
+  if (!toggle) {
+    return null;
+  }
   return (
     <div className="origin-top-right absolute right-0 mt-2 w-56
       rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -26,6 +30,7 @@ const DropDown = () => {
 }
 
 const Search = () => {
+  let [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
       <div className="mt-1 relative rounded-md shadow-sm">
@@ -37,11 +42,11 @@ const Search = () => {
           className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
         <div className="absolute inset-y-0 right-0 flex items-center">
           <div>
-            <button type="button"
+            <button type="button" onClick={() => { setToggleSort(!toggleSort) }}
               className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
               Sort By <BiCaretDown className="ml-2" />
             </button>
-            <DropDown />
+            <DropDown toggle={toggleSort} />
           </div>
         </div>
       </div>
