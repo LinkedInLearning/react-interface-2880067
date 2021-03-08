2c4c98306bd6edc1067408b4e66f237f30caee43
planetoftheweb <ray@planetoftheweb.com>
2021-03-07 23:49:23 -0500

    Appointment Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { BiArchive } from "react-icons/bi"
+import { BiCalendar } from "react-icons/bi"
 import Search from "./components/Search"
-
+import AddAppointment from "./components/AddAppointment"
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
+      <AddAppointment />
       <Search />
     </div>
   );
---
+++ src/components/AddAppointment.js
@@ -0,0 +1,73 @@
+import { BiCalendarPlus } from "react-icons/bi";
+
+const AddAppointment = () => {
+  return (
+    <div>
+      <button className="bg-blue-400 text-white px-2 py-3 w-full text-left rounded-t-md">
+        <div><BiCalendarPlus className="inline-block align-text-top" />  Add Appointment</div>
+      </button>
+      <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+          <label htmlFor="ownerName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            Owner Name
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <input type="text" name="ownerName" id="ownerName"
+              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+          </div>
+        </div>
+
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+          <label htmlFor="petName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            Pet Name
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <input type="text" name="petName" id="petName"
+              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+          </div>
+        </div>
+
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+          <label htmlFor="aptDate" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            Apt Date
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <input type="date" name="aptDate" id="aptDate"
+              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+          </div>
+        </div>
+
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+          <label htmlFor="aptTime" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            Apt Time
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <input type="time" name="aptTime" id="aptTime"
+              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+          </div>
+        </div>
+
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+          <label htmlFor="aptNotes" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            Appointment Notes
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <textarea id="aptNotes" name="aptNotes" rows="3"
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Detailed comments about the condition"></textarea>
+          </div>
+        </div>
+
+
+        <div className="pt-5">
+          <div className="flex justify-end">
+            <button type="submit" className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddAppointment
