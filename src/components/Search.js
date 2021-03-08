3766f5e5cf8287edcc168f02c6ed11a568de0138
planetoftheweb <ray@planetoftheweb.com>
2021-03-07 22:07:42 -0500

    Dropdown Sub-component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,29 @@
-import { BiSearch, BiCaretDown } from "react-icons/bi"
+import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi"
+
+const DropDown = () => {
+  return (
+    <div className="origin-top-right absolute right-0 mt-2 w-56
+      rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+      <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem">Pet Name <BiCheck /></div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem">Owner Name  <BiCheck /></div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem">Date <BiCheck /></div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+          role="menuitem">Asc <BiCheck /></div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem">Desc <BiCheck /></div>
+      </div>
+    </div>
+  )
+}
 
 const Search = () => {
   return (
@@ -16,6 +41,7 @@ const Search = () => {
               className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
               Sort By <BiCaretDown className="ml-2" />
             </button>
+            <DropDown />
           </div>
         </div>
       </div>
