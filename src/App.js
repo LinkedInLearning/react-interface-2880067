e277e29c86fc9396aaaf0a558a43c6692c8637d6
planetoftheweb <ray@planetoftheweb.com>
2021-03-09 01:27:18 -0500

    Adding Appointments

--- src/App.js
+++ src/App.js
@@ -43,7 +43,10 @@ function App() {
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
-      <AddAppointment />
+      <AddAppointment
+        onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}
+        lastId={appointmentList.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
+      />
       <Search query={query}
         onQueryChange={myQuery => setQuery(myQuery)}
         orderBy={orderBy}
--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -1,8 +1,30 @@
 import { BiCalendarPlus } from "react-icons/bi";
 import { useState } from 'react';
 
-const AddAppointment = () => {
+const AddAppointment = ({ onSendAppointment, lastId }) => {
+  const clearData = {
+    ownerName: '',
+    petName: '',
+    aptDate: '',
+    aptTime: '',
+    aptNotes: ''
+  }
   let [toggleForm, setToggleForm] = useState(false)
+  let [formData, setFormData] = useState(clearData)
+
+  function formDataPublish() {
+    const appointmentInfo = {
+      id: lastId + 1,
+      ownerName: formData.ownerName,
+      petName: formData.petName,
+      aptDate: formData.aptDate + ' ' + formData.aptTime,
+      aptNotes: formData.aptNotes
+    }
+    onSendAppointment(appointmentInfo);
+    setFormData(clearData);
+    setToggleForm(!toggleForm)
+  }
+
   return (
     <div>
       <button onClick={() => { setToggleForm(!toggleForm) }}
@@ -19,6 +41,8 @@ const AddAppointment = () => {
           </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="text" name="ownerName" id="ownerName"
+                onChange={(event) => { setFormData({ ...formData, ownerName: event.target.value }) }}
+                value={formData.ownerName}
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
             </div>
           </div>
@@ -29,6 +53,8 @@ const AddAppointment = () => {
           </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="text" name="petName" id="petName"
+                onChange={(event) => { setFormData({ ...formData, petName: event.target.value }) }}
+                value={formData.petName}
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
             </div>
           </div>
@@ -39,6 +65,8 @@ const AddAppointment = () => {
           </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="date" name="aptDate" id="aptDate"
+                onChange={(event) => { setFormData({ ...formData, aptDate: event.target.value }) }}
+                value={formData.aptDate}
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
             </div>
           </div>
@@ -49,6 +77,8 @@ const AddAppointment = () => {
           </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="time" name="aptTime" id="aptTime"
+                onChange={(event) => { setFormData({ ...formData, aptTime: event.target.value }) }}
+                value={formData.aptTime}
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
             </div>
           </div>
@@ -59,14 +89,15 @@ const AddAppointment = () => {
           </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <textarea id="aptNotes" name="aptNotes" rows="3"
+                onChange={(event) => { setFormData({ ...formData, aptNotes: event.target.value }) }}
+                value={formData.aptNotes}
                 className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Detailed comments about the condition"></textarea>
             </div>
           </div>
 
-
           <div className="pt-5">
             <div className="flex justify-end">
-              <button type="submit" className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
+              <button type="submit" onClick={formDataPublish} className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
                 Submit
             </button>
             </div>
