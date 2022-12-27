7c8ab584ae5b8cc18c87170647b4406cf32cfb96
walidja <walidhelmyjm@gmail.com>
2022-12-27 14:35:28 +0200

    First patch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BiArchive } from "react-icons/bi";
+import {} from "tailwindcss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {" "}
+        <BiArchive /> Appointments App
+      </h1>
     </div>
   );
 }
